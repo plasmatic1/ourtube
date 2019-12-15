2966a418ea066285d52f4ffffb83a29af742d485
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-12-14 20:36:33 -0500

    Add side panel.exe

---
+++ src/components/PlayistList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PlaylistList extends React.Component {
+    render() {
+        return (
+            <div>
+                {
+                    
+                }
+            </div>
+        );
+    }
+}
+
+export default PlaylistList;
