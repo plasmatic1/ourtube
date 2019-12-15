d63664615d74278f15d248658ed43e17ee38b8bf
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-12-14 22:49:11 -0500

    Test data lmao

---
+++ src/classes/Playlist.js
@@ -0,0 +1,8 @@
+class Playlist {
+    constructor(name, tracks) {
+        this.name = name;
+        this.tracks = tracks;
+    }
+}
+
+export default Playlist;
--- src/components/PlayistList.js
+++ src/components/PlayistList.js
@@ -3,11 +3,16 @@ import React from 'react';
 class PlaylistList extends React.Component {
     render() {
         return (
-            <div>
+            <ul>
                 {
-                    
+                    this.props.entries.map((v, i) => (
+                        <li>
+                            <h1>{v.title}</h1>
+                            <p>{v.artist} - {v.duration}</p>
+                        </li>
+                    ))
                 }
-            </div>
+            </ul>
         );
     }
 }
