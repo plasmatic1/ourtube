5b99fcc162e2313b8e80dcd1a571d1c3e676bbe8
Ray Wang <2022135@appleby.on.ca>
2019-12-14 21:47:01 -0500

    added fully working Track class

--- src/classes/track.js
+++ src/classes/track.js
@@ -1,11 +1,23 @@
 class Track{
-    length; //length of the song
-    artist; //name of the artist
-    name;
-
     constructor(length, artist, name){
-        this.length = length;
         this.artist = artist;
         this.name = name;
+        const hours = () => {
+            return String(Math.floor(length/60)) 
+        };
+        const minutes = () =>{
+            if (length%60 < 10)
+                return '0' + String(length%60)
+            return String(length%60)
+        }
+        this.length = hours() + ': ' + minutes();
     }
 }
+
+/*
+ EXAMPLE USAGE:
+*/
+const t = new Track(65, 'Justin Beiber', 'Despacito');
+console.log(t.length); //prints "1: 05"
+console.log(t.artist)
+console.log(t.name)
