a9113c459dfffbed57063cc39a8340cb51224911
Ray Wang <2022135@appleby.on.ca>
2019-12-14 20:53:30 -0500

    add a class to store information about tracks

---
+++ src/classes/track.js
@@ -0,0 +1,11 @@
+class track{
+    thumbnail; //the url to the image
+    length; //length of the song
+    artist; //name of the artist
+
+    constructor(thumbnail, length, artist){
+        this.thumbnail = thumbnail;
+        this.length = length;
+        this.artist = artist;
+    }
+}
